f581e90eadc9fbec6636c289d191f2c95ee6a9c9
Navya Agarwal <navyaagr025@gmail.com>
2023-11-10 15:19:20 -0600

    can move a point

--- visualization/joint_visualizations/visualize.c
+++ visualization/joint_visualizations/visualize.c
@@ -7,22 +7,24 @@
 #define maxWD 640
 #define maxHT 480
 
-// Rotation speed
-#define thetaSpeed 0.05
-
 // Animation parameters
 int hipX = 0.2124961254108306, hipY = 0.1269369842356025, hipdX = 0.2124961254108306 - 0.19589827348497596, hipdY = 0.1269369842356025 - 0.1262360727259671, frame_count = 3;
 
-// This creates delay between
-// two actions
+// Point coordinates
+int px = 100, py = 200;
+
+// Translation parameters
+int tx = 1, ty = 5;
+int translationCount = 0;
+
+// This creates delay between two actions
 void delay(unsigned int mseconds)
 {
     clock_t goal = mseconds + clock();
     while (goal > clock());
 }
 
-// This is a basic init for the
-// glut window
+// This is a basic init for the glut window
 void myInit(void)
 {
     glClearColor(1.0, 1.0, 1.0, 0.0);
@@ -33,63 +35,55 @@ void myInit(void)
     glFlush();
 }
 
-// This function just draws
-// a point
-void drawPoint(int x, int y)
+// This function just draws a point
+void drawPoint()
 {
     glPointSize(7.0);
     glColor3f(1.0f, 0.0f, 1.0f);
     glBegin(GL_POINTS);
-    glVertex2i(x, y);
+    glVertex2i(px, py);
     glEnd();
     glFlush();
 }
 
-// This function will translate
-// the point
-// need to call a function that graphs the differnet hip points
-void translatePoint(int px, int py,
-                    int tx, int ty)
-{
-    int fx = px, fy = py;
-    int counter = 5;
-    while (1)
-    {
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        // Update
-        px = px + tx;
-        py = py + ty;
-
-        // Check overflow to keep
-        // point in the screen
-        if (px > maxWD || px < 0 ||
-            py > maxHT || py < 0)
-        {
-            px = fx;
-            py = fy;
-        }
-
-        // Drawing the point
-        drawPoint(px, py);
-
-        glFlush();
-
-        // Creating a delay
-        // So that the point can be noticed
-        delay(10);
-    }
-}
-
+// This function will translate the point
+void translatePoint(int, int);
 
 // Actual display function
 void myDisplay()
 {
-    while (frame_count)
+    drawPoint();
+    translatePoint(tx, ty);
+}
+
+// Timer function for animation
+void timerFunction(int value)
+{
+    if (translationCount < 5)
     {
-        translatePoint(hipX, hipY, hipdX, hipdY);
-        frame_count--;
+        translatePoint(tx, ty);
+        glutTimerFunc(10, timerFunction, 0); // 1000 milliseconds delay
+        translationCount++;
     }
+}
+
+// This function will translate the point
+void translatePoint(int tx, int ty)
+{
+    px = px + tx;
+    py = py + ty;
+
+    // Check overflow to keep point in the screen
+    if (px > maxWD || px < 0 || py > maxHT || py < 0)
+    {
+        px = 100; // Reset to initial position
+        py = 200;
+    }
+
+    glClear(GL_COLOR_BUFFER_BIT);
+    drawPoint();
+    glFlush();
+    delay(1); // Adjust the delay if needed
 }
 
 // Driver code
@@ -102,6 +96,10 @@ int main(int argc, char **argv)
     glutCreateWindow("Transforming point");
     glutDisplayFunc(myDisplay);
     myInit();
+
+    // Start the timer for animation
+    glutTimerFunc(1000, timerFunction, 0);
+
     glutMainLoop();
     return 0;
 }
